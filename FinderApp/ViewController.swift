abc5c0b7d978daac32df7f08b33471dae2c89743
Andrey Krit <a.krit@seductive.com.ua>
2017-08-24 23:38:35 +0300

    Login screen added

---
+++ FinderApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinderApp
+//
+//  Created by Andrey Krit on 8/24/17.
+//  Copyright © 2017 Andrey Krit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
